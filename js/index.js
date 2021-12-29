ae1b42137479476c2243f32721ad0d8fbbf57d86
anastasia1756 <nastya7@ukr.net>
2021-12-29 22:37:25 +0300

    add functional

--- js/index.js
+++ js/index.js
@@ -5,13 +5,89 @@ const refs = {
   deleteBtn: document.querySelector('button[data-action="delete"]'),
   xmlBtn: document.querySelector('button[data-action="xml"]'),
   input: document.querySelector('.input'),
-  textarea: document.querySelector('.textarea'),
+  list: document.querySelector('.list'),
 };
+
+let sortName = [];
+let sortValue = [];
+refs.input.addEventListener('input', (e) => {
+  if (e.target.value) {
+    refs.addBtn.removeAttribute('disabled');
+  }
+  if (e.target.value === '') {
+    refs.addBtn.setAttribute('disabled', 'disabled');
+  }
+});
 
 const onAddBtnClick = (e) => {
   e.preventDefault();
-  console.log('add');
-  refs.textarea.value = refs.input.value;
+  refs.sortByValueBtn.removeAttribute('disabled');
+  refs.sortByNameBtn.removeAttribute('disabled');
+  refs.addBtn.setAttribute('disabled', 'disabled');
+  const input = refs.input.value.trim();
+  const valueIndex = input.indexOf('=') + 1;
+  const endNameIndex = input.indexOf('=');
+  const substrName = input.substring(0, endNameIndex).trim();
+  const substrValue = input.substring(valueIndex, input.length).trim();
+  const upperNameInput = substrName.replace(
+    substrName[0],
+    substrName[0].toUpperCase()
+  );
+  const upperValueInput = substrValue.replace(
+    substrValue[substrValue.indexOf('=') + 1],
+    substrValue[substrValue.indexOf('=') + 1].toUpperCase()
+  );
+
+  const markup = `<li class="list-item">${upperNameInput}=${upperValueInput}</li>`;
+  refs.list.insertAdjacentHTML('beforeend', markup);
   refs.input.value = '';
+  listItem = document.querySelectorAll('.list-item');
+
+  refs.sortByNameBtn.addEventListener('click', () => {
+    const pair = {
+      name: upperNameInput,
+      value: upperValueInput,
+    };
+    const firstWord = pair.name;
+    sortName.push(firstWord);
+
+    const uniqueSorted = sortName
+      .sort()
+      .filter((firstWord, index, array) => array.indexOf(firstWord) === index);
+    const sortByName = uniqueSorted.join('<br>');
+    const sortedNameList = `<li class="list-item">${sortByName}</li>`;
+    refs.list.innerHTML = sortedNameList;
+  });
+
+  refs.sortByValueBtn.addEventListener('click', () => {
+    const pair = {
+      name: upperNameInput,
+      value: upperValueInput,
+    };
+    const secondWord = pair.value;
+    sortValue.push(secondWord);
+    const uniqueValues = sortValue
+      .sort()
+      .filter(
+        (secondWord, index, array) => array.indexOf(secondWord) === index
+      );
+    pair.value = uniqueValues;
+    const sortByValue = pair.value.join('<br>');
+    const sortedValueList = `<li class="list-item">${sortByValue}</li>`;
+    refs.list.innerHTML = sortedValueList;
+  });
 };
 refs.addBtn.addEventListener('click', onAddBtnClick);
+
+// ====== Delete function ===== //
+
+const deleteClick = () => {
+  listItem = document.querySelector('.list-item');
+  if (listItem) {
+    listItem.remove();
+    refs.sortByNameBtn.setAttribute('disabled', 'disabled');
+    refs.sortByValueBtn.setAttribute('disabled', 'disabled');
+  }
+};
+
+refs.deleteBtn.addEventListener('click', deleteClick);
